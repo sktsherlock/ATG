5831d4360be4703cbece886aa5c63777f095d6a5
sktsherlock <1033270581@qq.com>
2023-11-21 22:30:06 +0800

    ATB 0.85 GAT OK

--- GNN/Library/GAT.py
+++ GNN/Library/GAT.py
@@ -1,18 +1,31 @@
-import torch 
+import torch as th
 import torch.nn as nn
 from dgl.utils import expand_as_pair
 from dgl import function as fn
 from dgl.ops import edge_softmax
+import sys
+import dgl.nn.pytorch as dglnn
+import argparse
+import wandb
+import numpy as np
+import torch.nn.functional as F
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from GraphData import load_data
+from NodeClassification import classification
+
 
 class ElementWiseLinear(nn.Module):
     def __init__(self, size, weight=True, bias=True, inplace=False):
         super().__init__()
         if weight:
-            self.weight = nn.Parameter(torch.Tensor(size))
+            self.weight = nn.Parameter(th.Tensor(size))
         else:
             self.weight = None
         if bias:
-            self.bias = nn.Parameter(torch.Tensor(size))
+            self.bias = nn.Parameter(th.Tensor(size))
         else:
             self.bias = None
         self.inplace = inplace
@@ -47,7 +60,6 @@ class GAT(nn.Module):
         n_heads,
         activation,
         dropout=0.0,
-        input_drop=0.0,
         attn_drop=0.0,
         edge_drop=0.0,
         use_attn_dst=True,
@@ -90,13 +102,11 @@ class GAT(nn.Module):
             n_classes, weight=False, bias=True, inplace=True
         )
 
-        self.input_drop = nn.Dropout(input_drop)
         self.dropout = nn.Dropout(dropout)
         self.activation = activation
 
     def forward(self, graph, feat):
         h = feat
-        h = self.input_drop(h)
 
         for i in range(self.n_layers):
             conv = self.convs[i](graph, h)
@@ -148,11 +158,11 @@ class GATConv(nn.Module):
                 self._in_src_feats, out_feats * num_heads, bias=False
             )
         self.attn_l = nn.Parameter(
-            torch.FloatTensor(size=(1, num_heads, out_feats))
+            th.FloatTensor(size=(1, num_heads, out_feats))
         )
         if use_attn_dst:
             self.attn_r = nn.Parameter(
-                torch.FloatTensor(size=(1, num_heads, out_feats))
+                th.FloatTensor(size=(1, num_heads, out_feats))
             )
         else:
             self.register_buffer("attn_r", None)
@@ -218,9 +228,9 @@ class GATConv(nn.Module):
 
             if self._use_symmetric_norm:
                 degs = graph.out_degrees().float().clamp(min=1)
-                norm = torch.pow(degs, -0.5)
+                norm = th.pow(degs, -0.5)
                 shp = norm.shape + (1,) * (feat_src.dim() - 1)
-                norm = torch.reshape(norm, shp)
+                norm = th.reshape(norm, shp)
                 feat_src = feat_src * norm
 
             # NOTE: GAT paper uses "first concatenation then linear projection"
@@ -245,10 +255,10 @@ class GATConv(nn.Module):
             e = self.leaky_relu(graph.edata.pop("e"))
 
             if self.training and self.edge_drop > 0:
-                perm = torch.randperm(graph.number_of_edges(), device=e.device)
+                perm = th.randperm(graph.number_of_edges(), device=e.device)
                 bound = int(graph.number_of_edges() * self.edge_drop)
                 eids = perm[bound:]
-                graph.edata["a"] = torch.zeros_like(e)
+                graph.edata["a"] = th.zeros_like(e)
                 graph.edata["a"][eids] = self.attn_drop(
                     edge_softmax(graph, e[eids], eids=eids)
                 )
@@ -261,9 +271,9 @@ class GATConv(nn.Module):
 
             if self._use_symmetric_norm:
                 degs = graph.in_degrees().float().clamp(min=1)
-                norm = torch.pow(degs, 0.5)
+                norm = th.pow(degs, 0.5)
                 shp = norm.shape + (1,) * (feat_dst.dim() - 1)
-                norm = torch.reshape(norm, shp)
+                norm = th.reshape(norm, shp)
                 rst = rst * norm
 
             # residual
@@ -278,3 +288,168 @@ class GATConv(nn.Module):
                 rst = self._activation(rst)
 
             return rst
+
+def args_init():
+    argparser = argparse.ArgumentParser(
+        "GAT Config",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    argparser.add_argument("--gpu", type=int, default=0, help="GPU device ID.")
+    argparser.add_argument(
+        "--n-runs", type=int, default=3, help="running times"
+    )
+    argparser.add_argument(
+        "--n-epochs", type=int, default=1000, help="number of epochs"
+    )
+    argparser.add_argument(
+        "--lr", type=float, default=0.005, help="learning rate"
+    )
+    argparser.add_argument(
+        "--n-layers", type=int, default=3, help="number of layers"
+    )
+    argparser.add_argument(
+        "--n-hidden", type=int, default=256, help="number of hidden units"
+    )
+    argparser.add_argument(
+        "--no-attn-dst", type=bool, default=True, help="Don't use attn_dst."
+    )
+    argparser.add_argument(
+        "--n-heads", type=int, default=3, help="number of heads"
+    )
+    argparser.add_argument(
+        "--attn-drop", type=float, default=0.0, help="attention drop rate"
+    )
+    argparser.add_argument(
+        "--edge-drop", type=float, default=0.0, help="edge drop rate"
+    )
+    argparser.add_argument(
+        "--dropout", type=float, default=0.5, help="dropout rate"
+    )
+    argparser.add_argument(
+        "--min-lr", type=float, default=0.0001, help="the min learning rate"
+    )
+    argparser.add_argument(
+        "--label-smoothing", type=float, default=0.1, help="the smoothing factor"
+    )
+    argparser.add_argument("--wd", type=float, default=0, help="weight decay")
+    # ! default
+    argparser.add_argument(
+        "--log-every", type=int, default=20, help="log every LOG_EVERY epochs"
+    )
+    argparser.add_argument(
+        "--eval_steps", type=int, default=1, help="eval in every epochs"
+    )
+    argparser.add_argument(
+        "--early_stop_patience", type=int, default=None, help="when to stop the  training loop to be aviod of the overfiting"
+    )
+    argparser.add_argument(
+        "--warmup_epochs", type=int, default=None, help="The warmup epochs"
+    )
+    # ! Data related
+    argparser.add_argument(
+        "--feature", type=str, default=None, help="Use LM embedding as feature", required=True
+    )
+    argparser.add_argument(
+        "--graph_path", type=str, default=None, help="The datasets to be implemented.", required=True
+    )
+    argparser.add_argument(
+        "--undirected", type=bool, default=False, help="Whether to undirect the graph."
+    )
+    argparser.add_argument(
+        "--selfloop", type=bool, default=False, help="Whether to add self loop in the graph."
+    )
+    argparser.add_argument(
+        "--metric", type=str, default='accuracy', choices=['accuracy', 'precision', 'recall', 'f1'],
+        help="The metric to be used."
+    )
+    argparser.add_argument(
+        "--average", type=str, default='weighted', choices=['weighted', 'micro', 'macro', None]
+    )
+    # ! Split datasets
+    argparser.add_argument(
+        "--train_ratio", type=float, default=0.6, help="training ratio"
+    )
+    argparser.add_argument(
+        "--val_ratio", type=float, default=0.2, help="training ratio"
+    )
+    return argparser
+
+def main():
+    argparser = args_init()
+    args = argparser.parse_args()
+    wandb.init(config=args, reinit=True)
+
+    device = th.device("cuda:%d" % args.gpu if th.cuda.is_available() else 'cpu')
+
+    # load data
+    graph, labels, train_idx, val_idx, test_idx = load_data(args.graph_path, train_ratio=args.train_ratio,
+                                                            val_ratio=args.val_ratio)
+
+    # add reverse edges, tranfer to the  undirected graph
+    if args.undirected:
+        print("The Graph change to the undirected graph")
+        srcs, dsts = graph.all_edges()
+        graph.add_edges(dsts, srcs)
+
+    # add self-loop
+    if args.selfloop:
+        print(f"Total edges before adding self-loop {graph.number_of_edges()}")
+        graph = graph.remove_self_loop().add_self_loop()
+        print(f"Total edges after adding self-loop {graph.number_of_edges()}")
+
+    feat = th.from_numpy(np.load(args.feature).astype(np.float32)).to(device)
+    n_classes = (labels.max()+1).item()
+    print(f"Number of classes {n_classes}, Number of features {feat.shape[1]}")
+
+    graph.create_formats_()
+
+    train_idx = train_idx.to(device)
+    val_idx = val_idx.to(device)
+    test_idx = test_idx.to(device)
+    print(f'Train_idx: {len(train_idx)}')
+    print(f'Valid_idx: {len(val_idx)}')
+    print(f'Test_idx: {len(test_idx)}')
+
+    labels = labels.to(device)
+    graph = graph.to(device)
+
+    # run
+    val_results = []
+    test_results = []
+
+    # Model implementation
+    model = GAT(
+        feat.shape[1],
+        n_classes,
+        args.n_hidden,
+        args.n_layers,
+        args.n_heads,
+        F.relu,
+        args.dropout,
+        args.attn_drop,
+        args.edge_drop,
+        not args.no_attn_dst,
+    ).to(device)
+
+    TRAIN_NUMBERS = sum(
+        [np.prod(p.size()) for p in model.parameters() if p.requires_grad]
+    )
+    print(f"Number of the all GNN model params: {TRAIN_NUMBERS}")
+
+    for run in range(args.n_runs):
+        model.reset_parameters()
+        val_result, test_result = classification(
+            args, graph, model, feat, labels, train_idx, val_idx, test_idx, run
+        )
+        wandb.log({f'Val {args.metric}': val_results, f'Test {args.metric}': test_results})
+        val_results.append(val_result)
+        test_results.append(test_result)
+
+    print(f"Runned {args.n_runs} times")
+    print(f"Average val accuracy: {np.mean(val_results)} ± {np.std(val_results)}")
+    print(f"Average test accuracy: {np.mean(test_results)} ± {np.std(test_results)}")
+    wandb.log({f'Mean_Val_{args.metric}': np.mean(val_results), f'Mean_Test_{args.metric}': np.mean(test_results)})
+
+
+if __name__ == "__main__":
+    main()
