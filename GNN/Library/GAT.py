390670303feae24bf12eb742fdd0510c5c5f8844
sktsherlock <1033270581@qq.com>
2023-11-22 10:48:24 +0800

    ATB 0.9 GAT OK

--- GNN/Library/GAT.py
+++ GNN/Library/GAT.py
@@ -448,7 +448,7 @@ def main():
         val_result, test_result = classification(
             args, graph, model, feat, labels, train_idx, val_idx, test_idx, run
         )
-        wandb.log({f'Val {args.metric}': val_result, f'Test {args.metric}': test_result})
+        wandb.log({f'Val_{args.metric}': val_result, f'Test_{args.metric}': test_result})
         val_results.append(val_result)
         test_results.append(test_result)
 
