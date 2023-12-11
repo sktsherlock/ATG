db028e340ec44e59032386b902148a309db53c23
sktsherlock <1033270581@qq.com>
2023-12-12 00:19:19 +0800

    ATB 1.9 Lora Debug

--- LM/E_BERT.py
+++ LM/E_BERT.py
@@ -285,6 +285,9 @@ def main():
     training_args.save_strategy = 'epoch'
     training_args.load_best_model_at_end = True
     training_args.save_total_limit = None
+    training_args.do_train = True
+    training_args.do_eval = True
+    training_args.do_predict = True
 
     send_example_telemetry("run_classification", model_args, data_args)
 
