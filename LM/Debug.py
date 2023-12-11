cef7a3cd639a6d7a21f0e44ae690bef8d8faba44
sktsherlock <1033270581@qq.com>
2023-12-11 11:15:55 +0800

    ATB 1.9 Lora Debug

---
+++ LM/Debug.py
@@ -0,0 +1,31 @@
+from transformers import AutoModelForSeq2SeqLM
+from peft import LoraModel, LoraConfig
+
+
+def print_trainable_parameters(model):
+    """
+    Prints the number of trainable parameters in the model.
+    """
+    trainable_params = 0
+    all_param = 0
+    for _, param in model.named_parameters():
+        all_param += param.numel()
+        if param.requires_grad:
+            trainable_params += param.numel()
+    print(
+        f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
+    )
+
+config = LoraConfig(
+    task_type="SEQ_2_SEQ_LM",
+    r=8,
+    lora_alpha=32,
+    target_modules=["q", "v"],
+    lora_dropout=0.01,
+)
+
+model = AutoModelForSeq2SeqLM.from_pretrained("t5-base")
+lora_model = LoraModel(model, config, "default")
+print(lora_model)
+print_trainable_parameters(lora_model)
+print_trainable_parameters(model)
