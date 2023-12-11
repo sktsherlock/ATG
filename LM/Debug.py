e731f98771767937fd9387457f867dcb7321bcb9
sktsherlock <1033270581@qq.com>
2023-12-11 13:16:34 +0800

    ATB 1.9 Lora Debug

--- LM/Debug.py
+++ LM/Debug.py
@@ -27,7 +27,7 @@ config = LoraConfig(
 model = AutoModel.from_pretrained('roberta-large')
 # model = AutoModelForSeq2SeqLM.from_pretrained("t5-base")
 print_trainable_parameters(model)
-# print([(n, type(m)) for n, m in model().named_modules()])
+print([(n, type(m)) for n, m in model.named_modules()])
 lora_model = LoraModel(model, config, "default")
 print_trainable_parameters(lora_model)
 print(lora_model)
