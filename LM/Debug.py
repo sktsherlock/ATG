fc62d9491fa5be09553751de2231e0f4aabe0555
sktsherlock <1033270581@qq.com>
2023-12-11 13:25:21 +0800

    ATB 1.9 Lora Debug

--- LM/Debug.py
+++ LM/Debug.py
@@ -1,27 +1,12 @@
 from transformers import AutoModel
 from peft import PeftModelForFeatureExtraction, get_peft_config
 
-def print_trainable_parameters(model):
-    """
-    Prints the number of trainable parameters in the model.
-    """
-    trainable_params = 0
-    all_param = 0
-    for _, param in model.named_parameters():
-        all_param += param.numel()
-        if param.requires_grad:
-            trainable_params += param.numel()
-    print(
-        f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
-    )
-
-
 config = {
     "peft_type": "LORA",
     "task_type": "FEATURE_EXTRACTION",
     "inference_mode": False,
     "r": 16,
-    "target_modules": ["query", "value"],
+    "target_modules": ["query", "value", "key"],
     "lora_alpha": 32,
     "lora_dropout": 0.05,
     "fan_in_fan_out": False,
@@ -31,6 +16,8 @@ peft_config = get_peft_config(config)
 model = AutoModel.from_pretrained("bert-base-cased")
 peft_model = PeftModelForFeatureExtraction(model, peft_config)
 peft_model.print_trainable_parameters()
+print(peft_model)
+
 
 
 
