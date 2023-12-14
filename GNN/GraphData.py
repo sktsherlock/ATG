fffd9e8d1f1308f3248c774d06009b57c78602ce
sktsherlock <1033270581@qq.com>
2023-12-14 19:12:32 +0800

    ATB 2.1 OGB Arxiv

--- GNN/GraphData.py
+++ GNN/GraphData.py
@@ -1,7 +1,7 @@
 import numpy as np
 import torch as th
 import dgl
-
+from ogb.nodeproppred import DglNodePropPredDataset
 
 def split_graph(nodes_num, train_ratio, val_ratio):
     np.random.seed(42)
@@ -16,13 +16,24 @@ def split_graph(nodes_num, train_ratio, val_ratio):
     return train_ids, val_ids, test_ids
 
 
-def load_data(graph_path, train_ratio=0.6, val_ratio=0.2):
-    # load the graph from local path
-    graph = dgl.load_graphs(graph_path)[0][0]
-    labels = graph.ndata['label']
-    train_idx, val_idx, test_idx = split_graph(graph.num_nodes(), train_ratio, val_ratio)
-    train_idx = th.tensor(train_idx)
-    val_idx = th.tensor(val_idx)
-    test_idx = th.tensor(test_idx)
+def load_data(graph_path, train_ratio=0.6, val_ratio=0.2, name=None):
+    if name == 'ogbn-arxiv':
+        data = DglNodePropPredDataset(name=name)
+        splitted_idx = data.get_idx_split()
+        train_idx, val_idx, test_idx = (
+            splitted_idx["train"],
+            splitted_idx["valid"],
+            splitted_idx["test"],
+        )
+        graph, labels = data[0]
+        labels = labels[:, 0]
+    else:
+        # load the graph from local path
+        graph = dgl.load_graphs(graph_path)[0][0]
+        labels = graph.ndata['label']
+        train_idx, val_idx, test_idx = split_graph(graph.num_nodes(), train_ratio, val_ratio)
+        train_idx = th.tensor(train_idx)
+        val_idx = th.tensor(val_idx)
+        test_idx = th.tensor(test_idx)
 
     return graph, labels, train_idx, val_idx, test_idx
