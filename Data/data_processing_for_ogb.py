9b45aed64ee43c0f7fb8ad05192c5da9b6129462
sktsherlock <1033270581@qq.com>
2023-12-14 19:00:36 +0800

    ATB 2.0 OGB Arxiv

--- Data/data_processing_for_ogb.py
+++ Data/data_processing_for_ogb.py
@@ -97,6 +97,7 @@ if __name__ == '__main__':
 
     dataset = DglNodePropPredDataset('ogbn-arxiv', root=data_root)
     _, label = dataset[0]
-    label_df = pd.DataFrame({'label_id': label.numpy()})
+    label_array = label.numpy().flatten()
+    label_df = pd.DataFrame({'label_id': label_array})
 
     main(raw_url=raw_text_url, data_path=os.path.join(data_root, 'ogbn_arxiv'))
