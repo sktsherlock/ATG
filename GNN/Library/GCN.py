deee898f7ceb35409f89989b8560c6bd004b3007
sktsherlock <1033270581@qq.com>
2023-12-14 22:24:07 +0800

    ATB 2.1 OGB Arxiv

--- GNN/Library/GCN.py
+++ GNN/Library/GCN.py
@@ -128,6 +128,9 @@ def args_init():
     )
     # ! Data related
     argparser.add_argument(
+        "--data_name", type=str, default=None, help="The dataset name.",
+    )
+    argparser.add_argument(
         "--feature", type=str, default=None, help="Use LM embedding as feature", required=True
     )
     argparser.add_argument(
@@ -165,7 +168,7 @@ def main():
 
     # load data
     graph, labels, train_idx, val_idx, test_idx = load_data(args.graph_path, train_ratio=args.train_ratio,
-                                                            val_ratio=args.val_ratio)
+                                                            val_ratio=args.val_ratio, name=args.data_name)
 
     # add reverse edges, tranfer to the  undirected graph
     if args.undirected:
