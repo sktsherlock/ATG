945063307a8d88e3cb263a35573b271d638b08e4
sktsherlock <1033270581@qq.com>
2023-12-21 21:03:04 +0800

    ATB 2.7 Adapter

--- GNN/Library/Adapter.py
+++ GNN/Library/Adapter.py
@@ -477,7 +477,7 @@ def main():
     # 创建保存路径
     save_path = os.path.join(args.teacher_path, args.data_name, args.teacher_name, feature_prefix)
     os.makedirs(save_path, exist_ok=True)
-    teacher_file_prefix = f"lr_{args.lr}_h_{args.teacher_n_hidden}_l_{args.teacher_layers}_h_{args.teacher_n_heads}"
+    teacher_file_prefix = f"lr_{args.teacher_lr}_h_{args.teacher_n_hidden}_l_{args.teacher_layers}_h_{args.teacher_n_heads}"
     # 保存 teacher model
     model_path = os.path.join(save_path, f"{teacher_file_prefix}.pth")
     teacher_model = teacher_training(args, teacher_model, graph, feat, labels, train_idx, val_idx, test_idx, model_path)
