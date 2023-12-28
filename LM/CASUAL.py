9b525a14c43aed59bc4890eae5034a4597ecd239
sktsherlock <1033270581@qq.com>
2023-12-29 02:15:54 +0800

    ATB 3.2 Prompt Generation

--- LM/CASUAL.py
+++ LM/CASUAL.py
@@ -169,7 +169,7 @@ Keywords:
 
     # Pipe 的方式生成并保存文本
     for out in tqdm(pipe(KeyDataset(prompt_dataset['train'], "TA"), do_sample=True, max_new_tokens=20, use_cache=True,
-                         repetition_penalty=2.5,
+                         repetition_penalty=1.0,
                          top_k=10, num_return_sequences=1, eos_token_id=tokenizer.eos_token_id,
                          return_full_text=False)):
         generated_text = out[0]['generated_text'] if args.task_name == "text-generation" else out[0]['summary_text']
