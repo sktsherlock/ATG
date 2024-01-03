1bf25f79cdcb6a1933dac42d30862c40e59c1930
sktsherlock <1033270581@qq.com>
2024-01-03 22:21:21 +0800

    ATB 3.6 Updated

--- Data/Statisitic.sh
+++ Data/Statisitic.sh
@@ -2,5 +2,6 @@
 
 csv_file=$1  # 获取命令行中的第一个参数作为csv_file
 column_name=$2  # 获取命令行中的第二个参数作为column_name
+threshold=$3
 
-python TextStatisitic.py --csv_file "$csv_file" --column_name "$column_name"
+python TextStatisitic.py --csv_file "$csv_file" --column_name "$column_name" --threshold "$threshold"
