f6a0e80eb36bcf2fdbb7a1990d450dcf6265908f
sktsherlock <1033270581@qq.com>
2024-01-03 21:36:18 +0800

    ATB 3.6 Updated

--- Data/TextStatisitic.py
+++ Data/TextStatisitic.py
@@ -7,10 +7,12 @@ def count_words(csv_file, column_name):
     data = pd.read_csv(csv_file)
     column_names = data.columns.tolist()
     print(column_names)
-    # 输出前几行
+    # 输出前后几行
     num_rows = 5  # 指定要输出的行数
     head_data = data.head(num_rows)
     print(head_data)
+    tail_data = data.tail(num_rows)
+    print(tail_data)
 
     if column_name == 'Debug':
         return
@@ -22,6 +24,13 @@ def count_words(csv_file, column_name):
     print(data['word_count'].describe())
     print('***************************')
     print(data['text_length'].describe())
+
+    max_length = data['text_length'].max()
+    max_length_row = data[data['text_length'] == max_length]
+    print(f'The max length row is in :{max_length_row}')
+    max_length_row_text = max_length_row[column_name].iloc[0]
+    print(max_length_row_text)
+
 
 
 
