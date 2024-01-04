2f9ad34dd9f8f2667e453c218154ac7dded00a15
sktsherlock <1033270581@qq.com>
2024-01-04 11:06:39 +0800

    ATB 3.6 Updated

--- GNN/Library/RevGAT.py
+++ GNN/Library/RevGAT.py
@@ -182,7 +182,7 @@ def main():
         set_seed(args.seed)
         model.reset_parameters()
         val_result, test_result = classification(
-            args, graph, model, feat, labels, train_idx, val_idx, test_idx, run
+            args, graph, model, feat, labels, train_idx, val_idx, test_idx, run+1
         )
         wandb.log({f'Val_{args.metric}': val_result, f'Test_{args.metric}': test_result})
         val_results.append(val_result)
