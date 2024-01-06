78cf55befcfbcc9fdbed81e48494e90cb83e21c0
sktsherlock <1033270581@qq.com>
2024-01-07 00:07:00 +0800

    ATB 3.7 Updated

--- GNN/LossFunction.py
+++ GNN/LossFunction.py
@@ -59,6 +59,18 @@ def _contrastive_loss_simsce(z1, z2, device, similarity='inner', temperature=0.1
     return loss_res
 
 
+
+def ncontrast(x_dis, adj_label, tau = 1):
+    """
+    compute the Ncontrast loss
+    """
+    x_dis = torch.exp( tau * x_dis)
+    x_dis_sum = torch.sum(x_dis, 1)
+    x_dis_sum_pos = torch.sum(x_dis*adj_label, 1)
+    loss = -torch.log(x_dis_sum_pos * (x_dis_sum**(-1))+1e-8).mean()
+    return loss
+
+
 class EarlyStopping:
     def __init__(self, patience=10):
         self.patience = patience
