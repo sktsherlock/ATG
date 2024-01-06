4631fc023f67da97e9f7024c591d3b90f0fcde73
sktsherlock <1033270581@qq.com>
2024-01-07 01:15:21 +0800

    ATB 3.7 Updated

--- GNN/Library/MLP.py
+++ GNN/Library/MLP.py
@@ -256,7 +256,7 @@ def main():
     device = th.device("cuda:%d" % args.gpu if th.cuda.is_available() else 'cpu')
 
     # load data
-    _, labels, train_idx, val_idx, test_idx = load_data(args.graph_path, train_ratio=args.train_ratio,
+    graph, labels, train_idx, val_idx, test_idx = load_data(args.graph_path, train_ratio=args.train_ratio,
                                                         val_ratio=args.val_ratio, name=args.data_name)
 
     feat = th.from_numpy(np.load(args.feature).astype(np.float32)).to(device) if args.feature is not None else graph.ndata['feat'].to(device)
