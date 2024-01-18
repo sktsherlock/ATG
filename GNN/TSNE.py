fef50716bfa5691850afa049f560edd8f155f74b
sktsherlock <1033270581@qq.com>
2024-01-18 22:39:36 +0800

    ATB 4.2 Updated

--- GNN/TSNE.py
+++ GNN/TSNE.py
@@ -43,7 +43,7 @@ device = th.device("cuda:%d" % gpu if th.cuda.is_available() else 'cpu')
 graph, labels, train_idx, val_idx, test_idx = load_data(graph_path=None, name='ogbn-arxiv')
 
 
-def visualize(feat1, feat2, path, label, sample_size=100, label1='PLM', label2='LLM'):
+def visualize(feat1, feat2, path, label, sample_size=200, label1='PLM', label2='LLM'):
     # 对 PLM_feat 进行采样和获取标签
     feat1_sample = feat1[:sample_size]
     # 对 LLM_feat 进行采样和获取标签
@@ -51,44 +51,39 @@ def visualize(feat1, feat2, path, label, sample_size=100, label1='PLM', label2='
     label_list = label[:sample_size]
 
     # 对 feat1 进行 TSNE 降维
-    tsne_feat1 = TSNE(n_components=3).fit_transform(feat1_sample)
+    tsne_feat1 = TSNE(n_components=2).fit_transform(feat1_sample)
 
     # 对 feat2 进行 TSNE 降维
-    tsne_feat2 = TSNE(n_components=3).fit_transform(feat2_sample)
+    tsne_feat2 = TSNE(n_components=2).fit_transform(feat2_sample)
 
     # 绘制 t-SNE 可视化结果并保存
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(tsne_feat1[:, 0], tsne_feat1[:, 1], tsne_feat1[:, 2], c=label_list, marker='*', label=label1, cmap='viridis')
-    ax.set_title(f'T-SNE Visualization for {label1}')
-    ax.legend()
+    plt.scatter(tsne_feat1[:, 0], tsne_feat1[:, 1], c=label_list, marker='*', label=label1, cmap='viridis')
+    plt.title(f'T-SNE Visualization for {label1}')
+    plt.legend()
     save_path_feat1 = os.path.join(path, f'{label1}_tsne.pdf')
     plt.savefig(save_path_feat1)
     plt.close()
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(tsne_feat2[:, 0], tsne_feat2[:, 1], tsne_feat2[:, 2], c=label_list, marker='o', label=label2, cmap='viridis')
-    ax.set_title(f'T-SNE Visualization for {label2}')
-    ax.legend()
+    plt.scatter(tsne_feat2[:, 0], tsne_feat2[:, 1], c=label_list, marker='o', label=label2, cmap='viridis')
+    plt.title(f'T-SNE Visualization for {label2}')
+    plt.legend()
     save_path_feat2 = os.path.join(path, f'{label2}_tsne.pdf')
     plt.savefig(save_path_feat2)
     plt.close()
 
-
     # 绘制 t-SNE 可视化结果
-    # plt.scatter(tsne_feat1[:, 0], tsne_feat1[:, 1], c=label_list, marker='*', label=label1, cmap='viridis')
-    # plt.scatter(tsne_feat2[:, 0], tsne_feat2[:, 1], c=label_list, marker='o', label=label2, cmap='viridis')
-    #
-    # # plt.scatter(tsne_result[:sample_size, 0], tsne_result[:sample_size, 1], cmap='viridis', label=label1)
-    # # plt.scatter(tsne_result[sample_size:, 0], tsne_result[sample_size:, 1], cmap='viridis', label=label2)
-    # # plt.xlabel('t-SNE Dimension 1')
-    # # plt.ylabel('t-SNE Dimension 2')
-    # plt.title(f'T-SNE Visualization between {label1} and {label2}')
-    # plt.legend()
-    # save_path = os.path.join(path, f'{label1}_{label2}_combined_tsne.pdf')
-    # plt.savefig(save_path)
-    # plt.show()
+    plt.scatter(tsne_feat1[:, 0], tsne_feat1[:, 1], c=label_list, marker='*', label=label1, cmap='viridis')
+    plt.scatter(tsne_feat2[:, 0], tsne_feat2[:, 1], c=label_list, marker='o', label=label2, cmap='viridis')
+
+    # plt.scatter(tsne_result[:sample_size, 0], tsne_result[:sample_size, 1], cmap='viridis', label=label1)
+    # plt.scatter(tsne_result[sample_size:, 0], tsne_result[sample_size:, 1], cmap='viridis', label=label2)
+    # plt.xlabel('t-SNE Dimension 1')
+    # plt.ylabel('t-SNE Dimension 2')
+    plt.title(f'T-SNE Visualization between {label1} and {label2}')
+    plt.legend()
+    save_path = os.path.join(path, f'{label1}_{label2}_combined_tsne.pdf')
+    plt.savefig(save_path)
+    plt.show()
 
 
 # def visualize(feat1, feat2, path, label, sample_size=2500, label1='PLM', label2='LLM'):
