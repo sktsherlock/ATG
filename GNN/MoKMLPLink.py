9021b27fd51b1de8f3df099122ad1308802b6637
sktsherlock <1033270581@qq.com>
2024-01-23 19:24:16 +0800

    ATB 4.2 Updated

--- GNN/MoKMLPLink.py
+++ GNN/MoKMLPLink.py
@@ -11,58 +11,61 @@ import dgl
 import numpy as np
 import wandb
 import os
+from LinkPrediction import SAGE, GCN, LinkPredictor
 
 
-class LinkPredictor(torch.nn.Module):
-    def __init__(self, in_channels, hidden_channels, out_channels, num_layers,
-                 dropout):
-        super(LinkPredictor, self).__init__()
-
-        self.lins = torch.nn.ModuleList()
-        self.lins.append(torch.nn.Linear(in_channels, hidden_channels))
-        for _ in range(num_layers - 2):
-            self.lins.append(torch.nn.Linear(hidden_channels, hidden_channels))
-        self.lins.append(torch.nn.Linear(hidden_channels, out_channels))
-
-        self.dropout = dropout
+class LPGNN(torch.nn.Module):
+    def __init__(self, LLM_in_feats, PLM_in_feats, alpha=0.5, conv='SAGE'):
+        super().__init__()
+        self.conv = conv
+        if conv == 'SAGE':
+            self.decomposition = SAGEConv(LLM_in_feats, PLM_in_feats, 'mean')
+        else:
+            self.decomposition = torch.nn.Linear(LLM_in_feats, PLM_in_feats)
+        self.alpha = alpha
 
     def reset_parameters(self):
-        for lin in self.lins:
-            lin.reset_parameters()
+        self.decomposition.reset_parameters()
 
-    def forward(self, x_i, x_j):
-        x = x_i * x_j
-        for lin in self.lins[:-1]:
-            x = lin(x)
-            x = F.relu(x)
-            x = F.dropout(x, p=self.dropout, training=self.training)
-        x = self.lins[-1](x)
-        return torch.sigmoid(x)
+    def forward(self, PLM_feat, LLM_feat, adj_t):  #
+        # Decomposition the LLM features
+        LLM_feat = self.decomposition(LLM_feat, adj_t) if self.conv == 'SAGE' else self.decomposition(LLM_feat)
+
+        # Trade off the LLM_feat and the PLM_feat
+
+        feat = self.alpha * LLM_feat + (1 - self.alpha) * PLM_feat
+        # Extract outputs from the model
+
+        return feat
 
 
-def train(predictor, x, split_edge, optimizer, batch_size):
+def train(model, predictor, PLM_feat, LLM_feat, adj_t, edge_split, optimizer, batch_size):
+    model.train()
     predictor.train()
 
-    pos_train_edge = split_edge['train']['edge'].to(x.device)
+    pos_train_edge = edge_split['train']['edge'].to(PLM_feat.device)
 
     total_loss = total_examples = 0
     for perm in DataLoader(range(pos_train_edge.size(0)), batch_size,
                            shuffle=True):
         optimizer.zero_grad()
 
+        h = model(PLM_feat, LLM_feat, adj_t)
+
         edge = pos_train_edge[perm].t()
 
-        pos_out = predictor(x[edge[0]], x[edge[1]])
+        pos_out = predictor(h[edge[0]], h[edge[1]])
         pos_loss = -torch.log(pos_out + 1e-15).mean()
 
         # Just do some trivial random sampling.
-        edge = torch.randint(0, x.size(0), edge.size(), dtype=torch.long,
-                             device=x.device)
-        neg_out = predictor(x[edge[0]], x[edge[1]])
+        edge = torch.randint(0, PLM_feat.size(0), edge.size(), dtype=torch.long,
+                             device=h.device)
+        neg_out = predictor(h[edge[0]], h[edge[1]])
         neg_loss = -torch.log(1 - neg_out + 1e-15).mean()
 
         loss = pos_loss + neg_loss
         loss.backward()
+
         optimizer.step()
 
         num_examples = pos_out.size(0)
@@ -73,43 +76,48 @@ def train(predictor, x, split_edge, optimizer, batch_size):
 
 
 @torch.no_grad()
-def test(predictor, x, split_edge, evaluator, batch_size):
+def test(model, predictor, PLM_feat, LLM_feat, adj_t, edge_split, evaluator, batch_size):
+    model.eval()
     predictor.eval()
 
-    pos_train_edge = split_edge['train']['edge'].to(x.device)
-    pos_valid_edge = split_edge['valid']['edge'].to(x.device)
-    neg_valid_edge = split_edge['valid']['edge_neg'].to(x.device)
-    pos_test_edge = split_edge['test']['edge'].to(x.device)
-    neg_test_edge = split_edge['test']['edge_neg'].to(x.device)
+    h = model(PLM_feat, LLM_feat, adj_t)
+
+    pos_train_edge = edge_split['train']['edge'].to(h.device)
+    pos_valid_edge = edge_split['valid']['edge'].to(h.device)
+    neg_valid_edge = edge_split['valid']['edge_neg'].to(h.device)
+    pos_test_edge = edge_split['test']['edge'].to(h.device)
+    neg_test_edge = edge_split['test']['edge_neg'].to(h.device)
 
     pos_train_preds = []
     for perm in DataLoader(range(pos_train_edge.size(0)), batch_size):
         edge = pos_train_edge[perm].t()
-        pos_train_preds += [predictor(x[edge[0]], x[edge[1]]).squeeze().cpu()]
+        pos_train_preds += [predictor(h[edge[0]], h[edge[1]]).squeeze().cpu()]
     pos_train_pred = torch.cat(pos_train_preds, dim=0)
 
     pos_valid_preds = []
     for perm in DataLoader(range(pos_valid_edge.size(0)), batch_size):
         edge = pos_valid_edge[perm].t()
-        pos_valid_preds += [predictor(x[edge[0]], x[edge[1]]).squeeze().cpu()]
+        pos_valid_preds += [predictor(h[edge[0]], h[edge[1]]).squeeze().cpu()]
     pos_valid_pred = torch.cat(pos_valid_preds, dim=0)
 
     neg_valid_preds = []
     for perm in DataLoader(range(neg_valid_edge.size(0)), batch_size):
         edge = neg_valid_edge[perm].t()
-        neg_valid_preds += [predictor(x[edge[0]], x[edge[1]]).squeeze().cpu()]
+        neg_valid_preds += [predictor(h[edge[0]], h[edge[1]]).squeeze().cpu()]
     neg_valid_pred = torch.cat(neg_valid_preds, dim=0)
+
+    h = model(PLM_feat, LLM_feat, adj_t)
 
     pos_test_preds = []
     for perm in DataLoader(range(pos_test_edge.size(0)), batch_size):
         edge = pos_test_edge[perm].t()
-        pos_test_preds += [predictor(x[edge[0]], x[edge[1]]).squeeze().cpu()]
+        pos_test_preds += [predictor(h[edge[0]], h[edge[1]]).squeeze().cpu()]
     pos_test_pred = torch.cat(pos_test_preds, dim=0)
 
     neg_test_preds = []
     for perm in DataLoader(range(neg_test_edge.size(0)), batch_size):
         edge = neg_test_edge[perm].t()
-        neg_test_preds += [predictor(x[edge[0]], x[edge[1]]).squeeze().cpu()]
+        neg_test_preds += [predictor(h[edge[0]], h[edge[1]]).squeeze().cpu()]
     neg_test_pred = torch.cat(neg_test_preds, dim=0)
 
     results = {}
@@ -134,22 +142,29 @@ def test(predictor, x, split_edge, evaluator, batch_size):
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Link-Prediction for MLP')
+    parser = argparse.ArgumentParser(description='Link-Prediction for GNN')
     parser.add_argument('--device', type=int, default=0)
     parser.add_argument('--log_steps', type=int, default=1)
-    parser.add_argument('--use_node_embedding', action='store_true')
     parser.add_argument('--num_layers', type=int, default=3)
     parser.add_argument('--hidden_channels', type=int, default=256)
     parser.add_argument('--dropout', type=float, default=0.0)
     parser.add_argument('--batch_size', type=int, default=64 * 1024)
-    parser.add_argument('--lr', type=float, default=0.01)
-    parser.add_argument('--epochs', type=int, default=200)
+    parser.add_argument('--lr', type=float, default=0.001)
+    parser.add_argument('--epochs', type=int, default=10)
+    parser.add_argument('--gnn_model', type=str, help='GNN MOdel', default='GCN')
     parser.add_argument('--eval_steps', type=int, default=1)
     parser.add_argument('--runs', type=int, default=5)
     parser.add_argument('--test_ratio', type=float, default=0.08)
     parser.add_argument('--val_ratio', type=float, default=0.02)
-    parser.add_argument('--neg_len', type=str, default='20000')
-    parser.add_argument("--feature", type=str, default='/dataintent/local/user/v-yinju/haoyan/Data/Movies/Feature/Movies_Llama_2_13b_hf_256_mean.npy', help="Use LM embedding as feature",)
+    parser.add_argument('--neg_len', type=str, default='2000')
+    parser.add_argument(
+        "--alpha", type=float, default=0.5, help="Tradeoff the LLM and the PLM"
+    )
+    parser.add_argument(
+        "--conv_type", type=str, default='SAGE', help="Use PLM embedding as feature"
+    )
+    parser.add_argument("--PLM_feature", type=str, default=None, help="Use LM embedding as feature",)
+    parser.add_argument("--LLM_feature", type=str, default=None, help="Use LLM embedding as feature")
     parser.add_argument("--path", type=str, default="/dataintent/local/user/v-yinju/haoyan/LinkPrediction/Movies/",
                         help="Path to save splitting")
     parser.add_argument("--graph_path", type=str, default="/dataintent/local/user/v-yinju/haoyan/Data/Movies/MoviesGraph.pt",
@@ -175,10 +190,18 @@ def main():
 
     edge_split = split_edge(graph, test_ratio=0.08, val_ratio=0.02, path=args.path, neg_len=args.neg_len)
 
-    x = torch.from_numpy(np.load(args.feature).astype(np.float32)).to(device)
+    PLM_feat = torch.from_numpy(np.load(args.PLM_feature).astype(np.float32)).to(device)
+    LLM_feat = torch.from_numpy(np.load(args.LLM_feature).astype(np.float32)).to(device)
 
 
-    predictor = LinkPredictor(x.size(-1), args.hidden_channels, 1,
+    edge_index = edge_split['train']['edge'].t()
+    adj_t = SparseTensor.from_edge_index(edge_index).t()
+    adj_t = adj_t.to_symmetric().to(device)
+
+
+    model = LPGNN(LLM_feat.shape[1], PLM_feat.shape[1], args.alpha, args.conv_type).to(device)
+
+    predictor = LinkPredictor(args.hidden_channels, args.hidden_channels, 1,
                               args.num_layers, args.dropout).to(device)
 
     evaluator = Evaluator(name='Movies')
@@ -189,14 +212,18 @@ def main():
     }
 
     for run in range(args.runs):
+        model.reset_parameters()
         predictor.reset_parameters()
-        optimizer = torch.optim.Adam(predictor.parameters(), lr=args.lr)
+        optimizer = torch.optim.Adam(
+            list(model.parameters()) + list(predictor.parameters()),
+            lr=args.lr)
 
         for epoch in range(1, 1 + args.epochs):
-            loss = train(predictor, x, edge_split, optimizer, args.batch_size)
+            loss = train(model, predictor, PLM_feat, LLM_feat, adj_t, edge_split, optimizer,
+                         args.batch_size)
 
             if epoch % args.eval_steps == 0:
-                results = test(predictor, x, edge_split, evaluator,
+                results = test(model, predictor, PLM_feat, LLM_feat, adj_t, edge_split, evaluator,
                                args.batch_size)
                 for key, result in results.items():
                     loggers[key].add_result(run, result)
