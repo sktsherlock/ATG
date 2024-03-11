2e2e0992fa696a42ec2d46c4cf076e25f33240fa
sktsherlock <1033270581@qq.com>
2024-03-11 15:24:04 +0800

    MAG 5.2 ebert Updated

--- FeatureExtractor/CLIP.py
+++ FeatureExtractor/CLIP.py
@@ -1,21 +1,52 @@
 from PIL import Image
-import requests
-
+import os
 from transformers import CLIPProcessor, CLIPModel
+import argparse
+import pandas as pd
 
-model = CLIPModel.from_pretrained("openai/clip-vit-large-patch14")
-processor = CLIPProcessor.from_pretrained("openai/clip-vit-large-patch14")
+parser = argparse.ArgumentParser(
+    description='Process text data and save the overall representation as an NPY file.')
+parser.add_argument('--model_name', type=str, default='openai/clip-vit-large-patch14',
+                    help='Name or path of the Huggingface model')
+parser.add_argument('--name', type=str, default='Movies', help='Prefix name for the  NPY file')
+parser.add_argument('--csv_path', type=str, default='./', help='Where save the picture')
+parser.add_argument('--path', type=str, default='./', help='Where save the picture')
+parser.add_argument('--max_length', type=int, default=128, help='Maximum length of the text for language models')
+parser.add_argument('--batch_size', type=int, default=1000, help='Number of batch size for inference')
+args = parser.parse_args()
 
-url = "https://images-na.ssl-images-amazon.com/images/I/61r5kg2QfhL.jpg"
-image = Image.open(requests.get(url, stream=True).raw)
+model = CLIPModel.from_pretrained(args.model_name)
+processor = CLIPProcessor.from_pretrained(args.model_name)
 
-inputs = processor(text=["a magazine belonging to the 'Arts, Music & Photography'", "a magazine belonging to the 'Fashion & Style'"], images=image, return_tensors="pt", padding=True)
+root_dir = os.path.dirname(os.path.abspath(__file__))
+base_dir = os.path.dirname(root_dir.rstrip('/'))
+picture_path = os.path.join(base_dir, args.path)
 
-inputs1 = processor(text="a magazine belonging to the:", images=image, return_tensors="pt", padding=True)
+df = pd.read_csv(args.csv_path)
+labels = df['second_category'].tolist()
+categories = df['second_category'].unique().tolist()
+print(categories)
 
-outputs = model(**inputs)
-outputs1 = model(**inputs1)
 
-logits_per_image = outputs.logits_per_image # this is the image-text similarity score
-probs = logits_per_image.softmax(dim=1) # we can take the softmax to get the label probabilities
-print('probs')
+
+
+for filename in os.listdir(picture_path):
+    if filename.endswith(".jpg") or filename.endswith(".png"):
+        print(filename)
+        image_path = os.path.join(picture_path, filename)
+        image = Image.open(image_path)
+
+        inputs = processor(text=[f"a {args.name} belonging to the '{category}'" for category in categories], images=image, return_tensors="pt", padding=True)
+        outputs = model(**inputs)
+        feature = outputs.image_embeds
+        logits_per_image = outputs.logits_per_image  # this is the image-text similarity score
+        probs = logits_per_image.softmax(dim=1)  # we can take the softmax to get the label probabilities
+
+
+
+
+        # 在这里处理每个图像的特征，可以将它们保存到一个列表或其他数据结构中
+
+print("已从文件夹中的所有图像中提取特征.")
+
+
