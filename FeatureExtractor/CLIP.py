e86fb63d43e1cc8a3ea70e43cd8dc94fa086d303
sktsherlock <1033270581@qq.com>
2024-03-26 17:02:32 +0800

    MAG 6.2 Updated

--- FeatureExtractor/CLIP.py
+++ FeatureExtractor/CLIP.py
@@ -77,7 +77,7 @@ if not os.path.exists(output_feature):
             image = Image.open(image_path)
 
             inputs = processor(text=[f"a {args.name} belonging to the '{category}'" for category in categories], images=image, return_tensors="pt", padding=True).to(device)
-            # print(f"a {args.name} belonging to the '{category}'" for category in categories)
+            print(f"a {args.name} belonging to the '{category}'" for category in categories)
             outputs = model(**inputs)
             feature = outputs.image_embeds
 
