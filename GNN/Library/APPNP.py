54b26cc6d3a24c4e83c80df2eea4911be3c8b499
sktsherlock <1033270581@qq.com>
2024-03-28 17:17:24 +0800

    MAG 6.6 setseed

--- GNN/Library/APPNP.py
+++ GNN/Library/APPNP.py
@@ -238,7 +238,7 @@ def main():
     print(f"Number of the all GNN model params: {TRAIN_NUMBERS}")
 
     for run in range(args.n_runs):
-        set_seed(args.seed) if args.fewshots is None else set_seed(args.seed + run)
+        set_seed(args.seed + run)
         model.reset_parameters()
         val_result, test_result = classification(
             args, graph, model, feat, labels, train_idx, val_idx, test_idx, run+1
