fabb582886b3561ab32db6255ab5fee980eb92e9
sktsherlock <1033270581@qq.com>
2024-04-10 20:10:02 +0800

    MAG 7.0 Reddit datasets

--- Data/data_processing_utils_for_reddit.py
+++ Data/data_processing_utils_for_reddit.py
@@ -178,7 +178,7 @@ def download_images(df, output_img_path):
                 break
             if need_deleted:
                 print('No.{} need to be deleted'.format(int(index)))
-        if (index + 1) % 100 == 0:
+        # if (index + 1) % 100 == 0:
             print('Downloaded {} items\' images, {} in total'.format(index + 1, total))
     print('Successfully downloaded images')
 
