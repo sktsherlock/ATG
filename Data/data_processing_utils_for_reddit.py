47ef276f71ad21c8af045e3bb7733a6aca6ac3b2
sktsherlock <1033270581@qq.com>
2024-04-10 20:41:50 +0800

    MAG 7.0 Reddit datasets

--- Data/data_processing_utils_for_reddit.py
+++ Data/data_processing_utils_for_reddit.py
@@ -64,17 +64,17 @@ def data_filter_for_reddit(df, category_number=50):
     df['subreddit'] = df['subreddit'].apply(lambda x: x if x in subreddit_to_keep else None)
     df.dropna(subset=['subreddit'], inplace=True)
 
-    # 进行采样
-    # 定义函数来对每个subreddit进行采样
-    def subreddit_sampling(group):
-        return group.sample(n=10, random_state=42)
+    # 对每个subreddit进行采样
+    new_df = pd.DataFrame()  # 创建一个空DataFrame用于存储采样结果
 
-    # 使用apply方法应用函数到原始DataFrame上
-    df = df.groupby('subreddit', group_keys=False).apply(subreddit_sampling)
-    print(df)
+    for subreddit in subreddit_to_keep:
+        subreddit_samples = df[df['subreddit'] == subreddit].sample(n=10, random_state=42)
+        new_df = new_df.append(subreddit_samples)
 
-    # 删除不包含指定subreddit的行
-    df.reset_index(drop=True, inplace=True)
+    # 重置索引并删除多余的列
+    new_df.reset_index(drop=True, inplace=True)
+    print(new_df)
+
 
     hash_set = {}
     for index, row in df.iterrows():
