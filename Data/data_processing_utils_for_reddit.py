423f5f358d449e0a10580761174bef10b810c1f8
sktsherlock <1033270581@qq.com>
2024-04-10 14:55:44 +0800

    MAG 7.0 Reddit datasets

--- Data/data_processing_utils_for_reddit.py
+++ Data/data_processing_utils_for_reddit.py
@@ -5,7 +5,10 @@ import time
 import pandas as pd
 import requests
 import warnings
-
+import argparse
+import torch
+import dgl
+import ast
 # 忽略特定警告
 warnings.filterwarnings("ignore", message="The frame.append method is deprecated and will be removed from pandas in a future version. Use pandas.concat instead.")
 
@@ -24,6 +27,20 @@ def parse_json(data_path):
         return df
     else:
         return None
+
+
+def count_data(df):
+    print('Counting data category...')
+    # 统计类别信息
+    print(df['subreddit'].value_counts())
+    print(df['label'].value_counts())
+
+    # 统计文本单词长度信息
+    print('Counting data text length...')
+    df2 = pd.DataFrame(df)
+    df2['text_length'] = df.apply(lambda x: len(x['caption'].split(' ')) if x['caption'] else 0, axis=1)
+    print(df2['text_length'].value_counts())
+    print(df2['text_length'].describe())
 
 
 # 数据过滤
@@ -70,9 +87,45 @@ def data_filter_for_reddit(df):
     df['label'] = df['subreddit'].map(hash_table)  # 类别映射为递增的 label
 
     # 只保留 DataFrame 中需要的列
-    df = df[['id', 'subreddit', 'caption', 'also_posted', 'label']]
+    df = df[['id', 'subreddit', 'caption', 'url', 'also_posted', 'label']]
 
     return df
+
+
+def construct_graph(input_csv_path, output_graph):
+
+    df = pd.read_csv(input_csv_path)
+
+    df['also_posted'] = df['also_posted'].apply(lambda x: ast.literal_eval(x))
+
+    df['neighbour'] = df['also_posted']
+    df['neighbour'] = df['neighbour'].apply(lambda x: list(set(x)))
+    df['neighbour'] = df['neighbour'].apply(lambda x: sorted(x))
+
+    adj_list = {}
+    asin_ids = sorted(df['id'].unique())
+    for asin_id in asin_ids:
+        adj_list[asin_id] = []
+    for index, row in df.iterrows():
+        src_asin_id = row['id']
+        neighbour_asins = row['neighbour']
+        for dest_asin in neighbour_asins:
+            adj_list[src_asin_id].append(dest_asin)
+    adj0 = []
+    adj1 = []
+    for src_asin_id, neighbors in adj_list.items():
+        for dest_asin_id in neighbors:
+            adj0.append(src_asin_id)
+            adj1.append(dest_asin_id)
+    # %%
+    adj0 = torch.tensor(adj0)
+    adj1 = torch.tensor(adj1)
+    # %%
+    G = dgl.graph((adj0, adj1))
+    G.ndata['label'] = torch.tensor(list(df['label']))
+    dgl.save_graphs(f"{output_graph}", G)
+    print(G)
+
 
 
 # 将 DataFrame 导出为 csv 文件
@@ -86,9 +139,9 @@ def download_images(df, output_img_path):
     print('Downloading images...')
     total = len(df)
     for index, row in df.iterrows():
-        if row['imageURLHighRes']:
+        if row['url']:
             need_deleted = True  # 是否需要删除该商品
-            for image_url in row['imageURLHighRes']:
+            for image_url in row['url']:
                 image_name = '{}.jpg'.format(int(index))  # 图像命名为 '商品id.jpg'
                 image_path = os.path.join(output_img_path, image_name)
                 if not os.path.exists(output_img_path):
@@ -107,31 +160,57 @@ def download_images(df, output_img_path):
     print('Successfully downloaded images')
 
 
-# df = parse_json('Reddit/annotations/abandoned_2017.json')
 
-folder_path = 'Reddit/annotations/'
-# 获取文件夹内所有文件名
-file_names = os.listdir(folder_path)
-data = pd.DataFrame(None, columns=['image_id', 'subreddit', 'url', 'caption', 'author'])
-for file_name in file_names:
-    print(file_name)
-    df = parse_json(os.path.join(folder_path, file_name))
-    if df is not None:
-        data = data.append(df)
 
-# 记录代码开始执行的时间
-start_time = time.time()
-data = data_filter_for_reddit(data)
-# 记录代码执行结束的时间
-end_time = time.time()
 
-# 计算代码执行的时间
-execution_time = end_time - start_time
 
-# 打印代码执行时间
-print("代码执行时间：", execution_time, "秒")
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--name', type=str, help='Dataset short name parameter', required=True)
+    parser.add_argument('--class_numbers', type=int, help='Dataset class numbers', required=True)
+    parser.add_argument('--download_image', action='store_true', help='whether to download the image')
+    parser.add_argument('--save', action="store_true", help="is saving or not")
+    args = parser.parse_args()
 
-print(data)
 
-# df = data_filter_for_reddit(df)
-# print(df)
+    name = args.name
+    class_numbers = args.class_numbers
+
+    if not os.path.exists(f'./{name}'):
+        os.makedirs(f'./{name}')
+
+    output_csv_path = f'./{name}/{name}.csv'
+    output_img = f'./{name}/{name}Images'
+    output_graph_path = f'./{name}/{name}Graph.pt'
+
+
+    folder_path = 'Reddit/annotations/'
+    # 获取文件夹内所有文件名
+    file_names = os.listdir(folder_path)
+    data = pd.DataFrame(None, columns=['image_id', 'subreddit', 'url', 'caption', 'author'])
+    for file_name in file_names:
+        df = parse_json(os.path.join(folder_path, file_name))
+        if df is not None:
+            data = data.append(df)
+
+    # 记录代码开始执行的时间
+    start_time = time.time()
+    data = data_filter_for_reddit(data)
+    # 记录代码执行结束的时间
+    end_time = time.time()
+    # 计算代码执行的时间
+    execution_time = end_time - start_time
+    # 打印代码执行时间
+    print("代码执行时间：", execution_time, "秒")
+    count_data(data)
+
+    if args.save is True:
+        export_as_csv(df, output_csv_path)
+        construct_graph(output_csv_path, output_graph_path)
+        # 从本地读取处理后的CSV文件
+        if args.download_image:
+            download_images(df, output_img)
+    else:
+        print('Check Finished.')
+    # df = data_filter_for_reddit(df)
+    # print(df)
