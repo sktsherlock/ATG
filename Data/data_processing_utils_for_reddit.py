da04155fa86f408265fd2c0101330e2e12aa1827
sktsherlock <1033270581@qq.com>
2024-04-10 20:37:06 +0800

    MAG 7.0 Reddit datasets

--- Data/data_processing_utils_for_reddit.py
+++ Data/data_processing_utils_for_reddit.py
@@ -70,7 +70,7 @@ def data_filter_for_reddit(df, category_number=50):
         return group.sample(n=10, random_state=42)
 
     # 使用apply方法应用函数到原始DataFrame上
-    df = df.groupby('subreddit').apply(subreddit_sampling)
+    df = df.groupby('subreddit', group_keys=False).apply(subreddit_sampling)
     print(df)
 
     # 删除不包含指定subreddit的行
