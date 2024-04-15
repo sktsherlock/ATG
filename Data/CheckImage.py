3cc6e8a3d01d3b1f68317816907dad38bb8f87e4
sktsherlock <1033270581@qq.com>
2024-04-15 21:26:19 +0800

    MAG 7.2

--- Data/CheckImage.py
+++ Data/CheckImage.py
@@ -2,16 +2,17 @@ import os
 from PIL import Image
 
 
+
 def check_image_files(image_folder):
     for filename in os.listdir(image_folder):
         if filename.endswith('.jpg'):
             file_path = os.path.join(image_folder, filename)
             try:
                 with Image.open(file_path) as img:
-                    # 图像文件能够被正确打开和识别
-                    pass
+                    width, height = img.size
+                    if width * height > 89478485:
+                        print('Image size超过限制的文件名:', filename)
             except (IOError, OSError, Image.UnidentifiedImageError):
-                # 图像文件无法被正确识别
                 print('无法识别的图像文件:', filename)
 
 # 指定图像文件夹路径
