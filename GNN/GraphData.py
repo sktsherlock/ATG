b2a0bc3e0729958e0867d607eaa4a8eb2dcda323
sktsherlock <1033270581@qq.com>
2024-04-24 21:12:04 +0800

    MAG 8.6 Link Prediction

--- GNN/GraphData.py
+++ GNN/GraphData.py
@@ -187,15 +187,15 @@ class Evaluator:
 
         # check the raw tyep of y_pred_pos
         if not (isinstance(y_pred_pos, np.ndarray) or (th is not None and isinstance(y_pred_pos, th.Tensor))):
-            raise ValueError('y_pred_pos needs to be either numpy ndarray or torch tensor')
+            raise ValueError('y_pred_pos needs to be either numpy ndarray or th tensor')
 
         # check the raw type of y_pred_neg
         if not (isinstance(y_pred_neg, np.ndarray) or (th is not None and isinstance(y_pred_neg, th.Tensor))):
-            raise ValueError('y_pred_neg needs to be either numpy ndarray or torch tensor')
+            raise ValueError('y_pred_neg needs to be either numpy ndarray or th tensor')
 
-        # if either y_pred_pos or y_pred_neg is torch tensor, use torch tensor
+        # if either y_pred_pos or y_pred_neg is th tensor, use th tensor
         if th is not None and (isinstance(y_pred_pos, th.Tensor) or isinstance(y_pred_neg, th.Tensor)):
-            # converting to torch.Tensor to numpy on cpu
+            # converting to th.Tensor to numpy on cpu
             if isinstance(y_pred_pos, np.ndarray):
                 y_pred_pos = th.from_numpy(y_pred_pos)
 
@@ -216,8 +216,11 @@ class Evaluator:
         if not y_pred_pos.ndim == 1:
             raise RuntimeError('y_pred_pos must to 1-dim arrray, {}-dim array given'.format(y_pred_pos.ndim))
 
-        if not y_pred_neg.ndim == 1:
-            raise RuntimeError('y_pred_neg must to 1-dim arrray, {}-dim array given'.format(y_pred_neg.ndim))
+        if not y_pred_neg.ndim == 2:
+            raise RuntimeError('y_pred_neg must to 2-dim arrray, {}-dim array given'.format(y_pred_neg.ndim))
+
+        return y_pred_pos, y_pred_neg, type_info
+
 
         return y_pred_pos, y_pred_neg, type_info
 
