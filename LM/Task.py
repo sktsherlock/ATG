7b83deab6543741c47cb1135807f879bc478208e
sktsherlock <1033270581@qq.com>
2024-05-21 10:33:18 +0800

    MAG 11 LM for NodeClassification

--- LM/Task.py
+++ LM/Task.py
@@ -58,71 +58,36 @@ class MEANClassifier(PreTrainedModel):
         return TokenClassifierOutput(loss=loss, logits=logits)
 
 
-class GAdapterClassifier(PreTrainedModel):
-    def __init__(self, model, n_labels, adapter, loss_func, resduial=True, dropout=0.0):
+
+class DualClassifier(PreTrainedModel):
+    def __init__(self, model, n_labels, loss_func, inputs_dim, dropout=0.0):
         super().__init__(model.config)
         self.encoder, self.loss_func = model, loss_func
-        self.adapter = adapter
         self.dropout = nn.Dropout(dropout)
         hidden_dim = model.config.hidden_size
-        self.resduial = resduial
+        self.alignment = nn.Linear(inputs_dim, hidden_dim)
         self.classifier = nn.Linear(hidden_dim, n_labels)
 
-    def forward(self, input_ids, attention_mask, labels):
+    def forward(self, input_ids, attention_mask, labels, nb_input_ids=None, nb_attention_mask=None, knowledge_embedding=None):
         # Extract outputs from the model
-        outputs = self.encoder(input_ids, attention_mask, output_hidden_states=True)
-        cls_emb = self.dropout(outputs.last_hidden_state[:, 0, :])
-        outputs, _ = self.adapter(cls_emb)
-        cls_emb = cls_emb + outputs if self.resduial else outputs
-        logits = self.classifier(cls_emb)
-        # [conv(mean_emb) + mean_emb] -> classifier  -> logits
+        if nb_input_ids is not None:
+            topology_ids = torch.cat([input_ids, nb_input_ids], dim=1)
+            topology_attention_mask = torch.cat([attention_mask, nb_attention_mask], dim=1)
+            text_representation = self.encoder.embeddings(topology_ids)
+
+            mean_emb = self.dropout(mean_pooling(outputs.last_hidden_state, topology_attention_mask))
+        else:
+
+            outputs = self.encoder(input_ids, attention_mask, output_hidden_states=True)
+            mean_emb = self.dropout(mean_pooling(outputs.last_hidden_state, attention_mask))
+            # mean_emb = self.dropout(torch.mean(outputs.last_hidden_state, dim=1))
+        logits = self.classifier(mean_emb)
         if labels.shape[-1] == 1:
             labels = labels.squeeze()
         loss = self.loss_func(logits, labels)
         return TokenClassifierOutput(loss=loss, logits=logits)
 
 
-class AdapterClassifier(PreTrainedModel):
-    def __init__(self, model, adapter, loss_func, dropout=0.0):
-        super().__init__(model.config)
-        self.encoder, self.loss_func = model, loss_func
-        self.adapter = adapter
-        self.dropout = nn.Dropout(dropout)
-
-    def forward(self, input_ids, attention_mask, labels):
-        # Extract outputs from the model
-        outputs = self.encoder(input_ids, attention_mask, output_hidden_states=True)
-        mean_emb = self.dropout(torch.mean(outputs.last_hidden_state, dim=1))
-        logits = self.adapter(mean_emb)
-        # [conv(mean_emb) + mean_emb] -> classifier  -> logits
-        if labels.shape[-1] == 1:
-            labels = labels.squeeze()
-        loss = self.loss_func(logits, labels)
-        return TokenClassifierOutput(loss=loss, logits=logits)
-
-
-class ResAdapterClassifier(PreTrainedModel):
-    def __init__(self, model, graphTuner, n_labels, loss_func, dropout=0.0, scale=1.0):
-        super().__init__(model.config)
-        self.encoder, self.loss_func = model, loss_func
-        self.GraphTuner = graphTuner
-        self.dropout = nn.Dropout(dropout)
-        hidden_dim = model.config.hidden_size
-        self.classifier = nn.Linear(hidden_dim, n_labels)
-        self.scale = scale
-
-    def forward(self, input_ids, attention_mask, labels):
-        # Extract outputs from the model
-        outputs = self.encoder(input_ids, attention_mask, output_hidden_states=True)
-        mean_emb = self.dropout(torch.mean(outputs.last_hidden_state, dim=1))
-        LLM_logits = self.classifier(mean_emb)
-        GraphTune_logits = self.GraphTuner(mean_emb)
-        logits = (1 - self.scale) * LLM_logits + self.scale * GraphTune_logits
-        # [conv(mean_emb) + mean_emb] -> classifier  -> logits
-        if labels.shape[-1] == 1:
-            labels = labels.squeeze()
-        loss = self.loss_func(logits, labels)
-        return TokenClassifierOutput(loss=loss, logits=logits)
 
 
 class TopologicalCL(PreTrainedModel):
