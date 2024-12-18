1f0a7632029441f296629cc5dc745da6e6d78c08
sktsherlock <1033270581@qq.com>
2024-12-18 11:01:46 +0800

    GNN Training Seed fix

--- GNN/GraphData.py
+++ GNN/GraphData.py
@@ -79,6 +79,8 @@ def set_seed(seed: int):
     th.cuda.manual_seed_all(seed)
     th.backends.cudnn.deterministic = True
     th.backends.cudnn.benchmark = False
+    th.use_deterministic_algorithms(True)
+    os.environ['CUBLAS_WORKSPACE_CONFIG'] = ':4096:8'
     os.environ['PYTHONHASHSEED'] = str(seed)
 
 
