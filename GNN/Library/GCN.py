d35533417a255c1c9b4dd0dd434e40d9bc9d8429
sktsherlock <1033270581@qq.com>
2024-12-23 16:19:25 +0800

    GNN Training Seed fix

--- GNN/Library/GCN.py
+++ GNN/Library/GCN.py
@@ -87,7 +87,7 @@ def main():
     args = argparser.parse_args()
     wandb.init(config=args, reinit=True)
 
-    device = th.device("cuda:%d" % args.gpu if th.cuda.is_available() else 'cpu')
+    device = th.device("cuda:%d" % args.gpu if th.cuda.is_available() and args.gpu != -1 else 'cpu')
 
     # load data
     graph, labels, train_idx, val_idx, test_idx = load_data(args.graph_path, train_ratio=args.train_ratio,
