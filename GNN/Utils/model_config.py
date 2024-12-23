86f4cac3ba1c49b4e1cbb8eab415d0dadcce872c
sktsherlock <1033270581@qq.com>
2024-12-23 17:12:21 +0800

    修正路径

--- GNN/Utils/model_config.py
+++ GNN/Utils/model_config.py
@@ -2,7 +2,7 @@ import torch.nn.functional as F
 import sys
 import os
 # sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from ..fusions.MultiBench import *
+from GNN.fusions.MultiBench import *
 
 
 
