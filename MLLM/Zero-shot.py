794dbad0bc812c4fbf9ed45ab6ad1e7bf6a43ee2
sktsherlock <1033270581@qq.com>
2025-02-07 23:51:53 +0800

    Update

--- MLLM/Zero-shot.py
+++ MLLM/Zero-shot.py
@@ -218,6 +218,9 @@ def main(args):
 
     # 如果使用 RAG 增强推理，转换 DGL 图为 NetworkX 图
     if args.k_hop > 0:
+        # 添加反向边，转换为无向图
+        srcs, dsts = dgl_graph.all_edges()
+        dgl_graph.add_edges(dsts, srcs)
         dgl_graph.ndata["_ID"] = torch.arange(dgl_graph.num_nodes())
         nx_graph = dgl.to_networkx(dgl_graph, node_attrs=['_ID'])  # 根据实际情况设置节点属性
     else:
