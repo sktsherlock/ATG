ed97d6c954248a2188c7bf21051beb82a4f1499c
sktsherlock <1033270581@qq.com>
2025-02-18 21:28:20 +0800

    Update

--- GNN/Utils/Homo.py
+++ GNN/Utils/Homo.py
@@ -1,7 +1,11 @@
 from tqdm import  tqdm
-from GNN.GraphData import load_data
 from collections import Counter
 import argparse
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from GNN.GraphData import load_data
+
 
 argparser = argparse.ArgumentParser(
     "Homo Config",
