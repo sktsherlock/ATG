d25e4827de031265665d89c28a0a85a50a8e2dbb
sktsherlock <1033270581@qq.com>
2025-02-18 21:33:13 +0800

    Update

--- GNN/Utils/Homo.py
+++ GNN/Utils/Homo.py
@@ -1,9 +1,32 @@
 from tqdm import  tqdm
 from collections import Counter
 import argparse
-import os
-import sys
-from GNN.GraphData import load_data
+import torch as th
+import dgl
+from ogb.nodeproppred import DglNodePropPredDataset
+
+
+def load_data(graph_path, train_ratio=0.6, val_ratio=0.2, name=None, fewshots=None,):
+    if name == 'ogbn-arxiv':
+        data = DglNodePropPredDataset(name=name)
+        splitted_idx = data.get_idx_split()
+        train_idx, val_idx, test_idx = (
+            splitted_idx["train"],
+            splitted_idx["valid"],
+            splitted_idx["test"],
+        )
+        graph, labels = data[0]
+        labels = labels[:, 0]
+    else:
+        # load the graph from local path
+        graph = dgl.load_graphs(graph_path)[0][0]
+        labels = graph.ndata['label']
+        train_idx, val_idx, test_idx = split_graph(graph.num_nodes(), train_ratio, val_ratio, labels, fewshots=fewshots)
+        train_idx = th.tensor(train_idx)
+        val_idx = th.tensor(val_idx)
+        test_idx = th.tensor(test_idx)
+
+    return graph, labels, train_idx, val_idx, test_idx
 
 
 argparser = argparse.ArgumentParser(
